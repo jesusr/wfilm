ad52189655b028c88412ca0b0d1ae249aba5ecb1
Jesús R Peinado <jesusrpeinado@gmail.com>
2016-05-14 00:05:45 +0200

    init

---
+++ client/main/main.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    moduleId: app.main,
+    selector: '#app',
+    templateUrl: 'main.component.html'
+})
+export class MainView implements OnInit {
+    constructor() { }
+
+    ngOnInit() { 
+        
+    }
+
+}
